a3b1612b4b59d5610246c6d029f8b0480c4e57cd
Hello-alt 242 <jbv77777@gmail.com>
2021-05-23 00:11:35 -0300

    modify category.js

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,7 @@
 const Category = require('../models/category')
 const Product = require('../models/product');
 const { errorHandler } = require("../helpers/dbErrorHandler");
-
+//gets the categories
 exports.categoryById = (req, res, next, id) => {
     Category.findById(id).exec((err, category) => {
         if (err || !category){
